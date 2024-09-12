132740a287cec295858e8374bb33fbe02490c2ae
Pedro Chaves <pedrochaves30@gmail.com>
2024-09-12 19:35:37 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -58,8 +58,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {//entra como falso (não marcado nas metas realizadas), mas precisa se tornar verdadeiro para que entre no filtro (entre como meta aberta)
+        return meta.checked != true//invertendo o valor de um booleano
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -82,6 +98,10 @@ const start= async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -102,6 +122,10 @@ const start= async () => {
                 await metasRealizadas()
                 break
             
+            case "abertas":
+                await metasAbertas()
+                break
+
             case "sair":
                 console.log("Até a próxima!")
                 return //não precisa de um break pois não possui nada mais abaixo do return
